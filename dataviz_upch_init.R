78c8276cd237675a2e245eae6b1161ebebaa3d2d
gcarrasco <gabc91@gmail.com>
2020-10-09 22:10:37 -0700

    Update dataviz_upch_init.R

--- dataviz_upch_init.R
+++ dataviz_upch_init.R
@@ -1,1 +1,7 @@
-install.packages(c("tidyverse", "colorspace"))
+x <- read.csv("https://raw.githubusercontent.com/gcarrascoe/dataviz_upch/main/list_pckg.csv", stringsAsFactors = F)
+
+pckg <- unlist(x)
+
+install.packages(pckg)
+
+rm(x, pckg)
